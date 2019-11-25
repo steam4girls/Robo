e40c993c99befd21b0bab740a2ab13c9d179a56a
steam4girls <55408033+steam4girls@users.noreply.github.com>
2019-11-25 11:18:48 -0300

    Update seguidor.ino

--- seguidor.ino
+++ seguidor.ino
@@ -1,15 +1,14 @@
 /*DECLARAÇÃO DE VARIAVEIS*/
 int IN1 = 4;
 int IN2 = 5;
-int MotorA_PWM = 3;
 int IN3 = 6;
 int IN4 = 7; 
-int MotorB_PWM = 9;  
+
 
 #define Sensor_direita 12
 #define Sensor_esquerda 13
  
-bool direita, esquerda;
+bool sens_direita, sens_esquerda;
  
 void setup() {
  Serial.begin(9600);
@@ -22,48 +21,68 @@ void setup() {
  //pinMode(MotorB_PWM, OUTPUT);
  pinMode(Sensor_direita, INPUT);
  pinMode(Sensor_esquerda, INPUT);
-  
-}
+ delay(1000);
  
-void loop() {
-   //Define o sentido de rotação dos motores
+}
+
+void frente(){
+   digitalWrite(IN1, LOW);
+   digitalWrite(IN2, HIGH);
+   digitalWrite(IN3, HIGH);
+   digitalWrite(IN4, LOW);
+}
+
+void esquerda(){
+   digitalWrite(IN1, LOW);
+   digitalWrite(IN2, HIGH);
+   digitalWrite(IN3, LOW);
+   digitalWrite(IN4, HIGH);
+}
+
+void direita(){
    digitalWrite(IN1, HIGH);
    digitalWrite(IN2, LOW);
    digitalWrite(IN3, HIGH);
    digitalWrite(IN4, LOW);
-   analogWrite(MotorA_PWM, 250);
-  analogWrite(MotorB_PWM, 250); 
+}
+void parar(){
+   digitalWrite(IN1, LOW);
+   digitalWrite(IN2, LOW);
+   digitalWrite(IN3, LOW);
+   digitalWrite(IN4, LOW);
+}
+void fim(){
+   while(1);
+}
+
+void loop() {
+   //Define o sentido de rotação dos motores
+
   //Leituras dos Sensores
-  direita = digitalRead(Sensor_direita);
-  esquerda = digitalRead(Sensor_esquerda);
-  Serial.print(direita);
+  sens_direita = digitalRead(Sensor_direita);
+  sens_esquerda = digitalRead(Sensor_esquerda);
+  Serial.print(sens_direita);
   Serial.print(" || ");
-  Serial.println(esquerda);
+  Serial.println(sens_esquerda);
  
   //Rodando os motores dependendo das leituras
- if(direita == false && esquerda == false){
- analogWrite(MotorA_PWM, 250);
- analogWrite(MotorB_PWM, 250);
- Serial.print("Frente");
-
- } else if(direita == false && esquerda == true){
- //delay(400);
- analogWrite(MotorA_PWM, 250);
- analogWrite(MotorB_PWM, 200);
- Serial.print("Direita");
- //delay(400);
- }else if(direita == true && esquerda == false){
- //delay(400);
- analogWrite(MotorA_PWM, 200);
- analogWrite(MotorB_PWM, 250);
- //delay(400);
- Serial.print("Esqueda");
- 
- }else if(direita == true && esquerda == true){
- analogWrite(MotorA_PWM, 0);
- analogWrite(MotorB_PWM, 0);
- Serial.print("Parar");
- while(1);
- }
-
+if(sens_direita == false && sens_esquerda == false){
+  Serial.print("Frente");
+  frente();
+}
+if(sens_direita == false && sens_esquerda == true){
+  Serial.print("Direita");
+  direita();
+  delay(50);
+}
+if(sens_direita == true && sens_esquerda == false){
+  Serial.print("Esqueda");
+  esquerda();
+  delay(50);
+}
+if(sens_direita == true && sens_esquerda == true){
+  Serial.print("Parar");
+  parar();
+  fim();
+}
 }
